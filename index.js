a386716bb64e96566b9da87ff16288f96493949e
vishwa <vishwakr1998@gmail.com>
2023-05-19 18:30:17 +0530

    signed

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const mongodb = require('mongodb');
+const mongoClient = mongodb.MongoClient;
+const url = "mongodb+srv://Vishwa:vishwa@cluster0.wckof1g.mongodb.net/?retryWrites=true&w=majority"
+
+
+
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use(cors({
+    origin: "*"
+}));
+
+
+app.post('/users', async (req, res) => {
+    try {
+        let connection = await mongoClient.connect(url);
+        let db = connection.db('mvg');
+        let user = await db.collection('register').findOne({ email: req.body.email })
+        if (user) {
+            res.json({ message: "already have" })
+        } else {
+            let collection = await db.collection('register').insertOne(req.body)
+            res.json({ message: "submit" })
+        }
+        let close = await connection.close()
+        res.json({message:"Success"})
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+app.listen(3088, () => {
+    console.log("server is started in 3088")
+})
+
+
+
+
+
+
+
+
+
